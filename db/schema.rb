c131115fc0e237cd98ed8f57e5ff132bc519f1ce
anton <ant.punko@gmail.com>
2016-03-25 21:35:29 +0300

    Add Game model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324211315) do
+ActiveRecord::Schema.define(version: 20160325183036) do
+
+  create_table "games", force: :cascade do |t|
+    t.string   "jsongame",     limit: 255
+    t.boolean  "computermove"
+    t.boolean  "useriswhite"
+    t.integer  "user_id",      limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -33,4 +44,5 @@ ActiveRecord::Schema.define(version: 20160324211315) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "games", "users"
 end
