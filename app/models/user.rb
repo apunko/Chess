48ea09db768b1709f58aa19be4e3087d8a8d6941
anton <ant.punko@gmail.com>
2016-03-25 21:33:42 +0300

    Add index and show users pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :games
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = "#{uid}@#{provider}.com"
